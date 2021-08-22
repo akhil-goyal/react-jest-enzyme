19e9d8f5e4881121101ddecb6cad8814edc06ace
Akhil Goyal <akhilgoyal@akhils-mbp.lan>
2021-08-22 00:39:09 -0400

    Added tests for comments box component.

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,5 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => {
-    return <div>Comment Box</div>
+class CommentBox extends Component {
+
+    state = { comment: '' };
+
+    handleChange = (event) => {
+        this.setState({ comment: event.target.value });
+    }
+
+    handleSubmit = (event) => {
+
+        event.preventDefault();
+
+        // CTA
+
+        this.setState({ comment: '' });
+
+    }
+
+    render() {
+
+        const { comment } = this.state;
+
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <h4>Add a comment</h4>
+                <textarea onChange={this.handleChange} value={comment} />
+                <div>
+                    <button>Submit</button>
+                </div>
+            </form>
+        )
+    }
 }
+
+export default CommentBox;
--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -4,13 +4,17 @@ import App from 'components/App';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
 
+let wrapped;
+
+beforeEach(() => {
+    wrapped = shallow(<App />);
+});
+
 it('shows a comment box', () => {
-    const wrapped = shallow(<App />);
     // console.log((wrapped.find(CommentBox)));
     expect(wrapped.find(CommentBox).length).toEqual(1);
 });
 
 it('shows a comments list', () => {
-    const wrapped = shallow(<App />);
     expect(wrapped.find(CommentList).length).toEqual(1);
 });
